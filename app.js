f02e0fdc4b47f6b51715dc9f8978b064d445425a
Elia <eliadallanoce@gmail.com>
2020-05-20 14:38:52 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const express= require('express');
+const app= express();
+var port = process.env.PORT || 3000;
+app.get('/', (req, res)=> res.send("ciaoo"));
+app.listen(port, ()=> console.log('server in ascolto:'+ port));
